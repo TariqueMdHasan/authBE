6d412c38b3d887a980ff2f30ca516d8dc3197c0c
Md Tarique Hasan <TariqueMdHasan@gmail.com>
2024-12-27 05:30:24 +0530

    jwt spelling changed

--- middleware/authMiddleware.js
+++ middleware/authMiddleware.js
@@ -4,13 +4,14 @@ const authMiddleware = (req, res, next) => {
     const token = req.header('Authorizartion')?.split(' ')[1];
     try{
         if(!token){
-            return res.status(401).json({ message: 'Unauthorize' });
+            return res.status(401).json({ message: 'Unauthorized: token is missing' });
         }
-        const decoded = jwt.verify(token, process.env.jwt_SECRET);
+        const decoded = jwt.verify(token, process.env.JWT_SECRET);
         req.user = decoded;
         next();
     }catch(error){
         console.log('authMiddleware getting some error', error);
+        return res.status(401).json({ message: 'Unauthorized: token is invalid' });
     }
 }
 
